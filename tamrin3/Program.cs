604b392490fe494cf1dedf00cddea99bda204a4b
User <User@DESKTOP-1364VBG>
2024-01-29 17:43:30 +0330

    Add project files.

---
+++ tamrin3/Program.cs
@@ -0,0 +1,19 @@
+﻿
+    using System;
+namespace project
+{
+    class test
+    {
+        public static void main()
+        {
+            double x = Convert.ToDouble(Console.ReadLine());
+            double Result = num(x);
+            Console.WriteLine(Result);
+            double num(double x)
+            {
+                double Res = Math.Pow(x, 2) - 2 * x + 4;
+                return Res;
+            }
+        }
+    }
+}
